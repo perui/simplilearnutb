a30c64f68f6df498c889ba302629424aa2d01f41
Ulrika Häggqvist <ulrika.haggqvist@arbetsformedlingen.se>
2024-06-28 13:19:16 +0200

    update

---
+++ js/validation.js
@@ -0,0 +1,9 @@
+
+function validation(){
+    let username = getElementById('username');
+    let pwd = getElementById('password');
+
+    if (username !== "") {
+        window.alert("Login failed");
+    }
+}
